04f335574308bd6847ffb13704f537e20d96ed05
Robert Bragg <robert.bragg@intel.com>
2015-09-07 23:26:39 +0100

    Simplify autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,38 +1,14 @@
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+#! /bin/sh
 
-srcdir=`dirname $0`
+srcdir=`dirname "$0"`
 test -z "$srcdir" && srcdir=.
 
-cd $srcdir
-PROJECT=clib
+ORIGDIR=`pwd`
+cd "$srcdir"
 
-test -f clib/clib.h || {
-	echo "You must run this script in the top-level $PROJECT directory"
-	exit 1
-}
+autoreconf --force --verbose --install || exit 1
+cd "$ORIGDIR" || exit $?
 
-AUTOMAKE_VERSIONS="1.15 1.14"
-for version in $AUTOMAKE_VERSIONS; do
-	if automake-$version --version < /dev/null > /dev/null 2>&1 ; then
-		AUTOMAKE=automake-$version
-		ACLOCAL=aclocal-$version
-		export AUTOMAKE ACLOCAL
-		break
-	fi
-done
-
-if test -z "$AUTOMAKE"; then
-	echo
-	echo "You must have one of automake $AUTOMAKE_VERSIONS to compile $PROJECT."
-	echo "Install the appropriate package for your distribution,"
-	exit 1
+if test -z "$NOCONFIGURE"; then
+    "$srcdir"/configure "$@"
 fi
-
-rm -rf autom4te.cache
-
-autoreconf -vfi || exit $?
-
-./configure "$@" || exit $?
-
-echo "Now type 'make' to compile $PROJECT."
